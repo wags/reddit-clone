8d9f93ec4839659019c0ddbddf1a72b2ac7c3fad
Matt Wagner <matt@wags.us>
2017-05-21 11:09:26 -0400

    Demonstrate Angular component basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Hello {{ title }}</h1>
+  `
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Matt';
 }
