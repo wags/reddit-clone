138094054276fda2e1037c85d4d6d43ed36ba6d2
Matt Wagner <matt@wags.us>
2017-05-22 16:54:22 -0400

    Move component code into individual files
    Using `ng generate component <name>`, create dedicated Angular components for the sidebar and article. Fill in the respective files with code from app.component.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import {
-  AppComponent,
-  SidebarComponent,
-  ArticleComponent
-} from './app.component';
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
