5887fe80c486cd458b31c40f27a9a210b80aa915
Matt Wagner <matt@wags.us>
2017-05-23 10:45:32 -0400

    Configure required parameters for the API request

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, URLSearchParams } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 import { Article } from './article';
 
 const baseUrl = 'https://newsapi.org';
+const newsApiKey = '3f42a565c14e4b1a87456bde7ec4faca';
 
 @Injectable()
 export class ArticleService {
@@ -14,8 +15,14 @@ export class ArticleService {
   ) { }
 
   public getArticles(): Promise<Article[]> {
+    const params = new URLSearchParams();
+    params.set('apiKey', newsApiKey);
+    params.set('source', 'reddit-r-all');
+
     return this.http
-      .get(`${baseUrl}/v1/articles`)
+      .get(`${baseUrl}/v1/articles`, {
+        search: params
+      })
       .toPromise()
       .then(resp => resp.json())
       .then(json => {
