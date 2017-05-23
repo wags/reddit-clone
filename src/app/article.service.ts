74ef6df2083e5cabae5a458786345170e62ce239
Matt Wagner <matt@wags.us>
2017-05-23 09:39:43 -0400

    Add a delay to show data loading via promise
    The article service creates a new promise that gets returned, calls "then" on that promise, and then it waits 2 seconds (using the plain JavaScript "setTimeout" method), then calls the function "resolve" in the promise to return the array of articles.

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -7,24 +7,28 @@ export class ArticleService {
   constructor() { }
 
   public getArticles(): Promise<Article[]> {
-    return Promise.resolve([
-      new Article(
-        'The Angular 2 screencast',
-        'The easiest way to learn Angular 2 is with Fullstack.io!',
-        10
-      ),
-      new Article(
-        'Fullstack React',
-        'Want to learn React too?'
-      ),
-      new Article(
-        'Vue is new',
-        'And pretty cool syntax too'
-      ),
-      new Article(
-        'But what about elm?',
-        'Everybody likes elm'
-      )
-    ]);
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve([
+          new Article(
+            'The Angular 2 screencast',
+            'The easiest way to learn Angular 2 is with Fullstack.io!',
+            10
+          ),
+          new Article(
+            'Fullstack React',
+            'Want to learn React too?'
+          ),
+          new Article(
+            'Vue is new',
+            'And pretty cool syntax too'
+          ),
+          new Article(
+            'But what about elm?',
+            'Everybody likes elm'
+          )
+        ]);
+      }, 2000);
+    });
   }
 }
