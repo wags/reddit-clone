383b3a505551b24e558001b8a8e3740cf30e4056
Matt Wagner <matt@wags.us>
2017-05-23 09:16:25 -0400

    Move article data into the article service
    Decouple the data responsibility from the app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,24 +13,6 @@ export class AppComponent {
   constructor(
     private articleService: ArticleService
   ) {
-    this.articles = [
-      new Article(
-        'The Angular 2 screencast',
-        'The easiest way to learn Angular 2 is with Fullstack.io!',
-        10
-      ),
-      new Article(
-        'Fullstack React',
-        'Want to learn React too?'
-      ),
-      new Article(
-        'Vue is new',
-        'And pretty cool syntax too'
-      ),
-      new Article(
-        'But what about elm?',
-        'Everybody likes elm'
-      )
-    ];
+    this.articles = articleService.getArticles();
   }
 }
--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Article } from './article';
 
 @Injectable()
 export class ArticleService {
 
   constructor() { }
 
+  public getArticles(): Article[] {
+    return [
+      new Article(
+        'The Angular 2 screencast',
+        'The easiest way to learn Angular 2 is with Fullstack.io!',
+        10
+      ),
+      new Article(
+        'Fullstack React',
+        'Want to learn React too?'
+      ),
+      new Article(
+        'Vue is new',
+        'And pretty cool syntax too'
+      ),
+      new Article(
+        'But what about elm?',
+        'Everybody likes elm'
+      )
+    ];
+  }
 }
