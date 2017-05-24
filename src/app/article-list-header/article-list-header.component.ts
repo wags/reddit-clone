405afbd98dd96e88beb8f80ef798fe109d92ac05
Matt Wagner <matt@wags.us>
2017-05-24 13:51:32 -0400

    Add article list header component
    Add a new component called article list header and some initial code to display buttons for direction and filtering at the top of the list of articles.

---
+++ src/app/article-list-header/article-list-header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-list-header',
+  templateUrl: './article-list-header.component.html',
+  styleUrls: ['./article-list-header.component.css']
+})
+export class ArticleListHeaderComponent implements OnInit {
+
+  private currentFilter: string = 'Time';
+  private sortDirection: number = 1;
+
+  constructor() { }
+
+  changeDirection() {
+    // update the direction
+  }
+
+  changeSort(filter: string) {
+    // update the filter
+  }
+
+  ngOnInit() {
+  }
+
+}
