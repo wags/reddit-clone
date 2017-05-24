0450f6dcb69811f70f41ed93b05e7411421834be
Matt Wagner <matt@wags.us>
2017-05-24 13:15:36 -0400

    Convert Promise to an RxJS Observable

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,37 +1,47 @@
 import { Injectable } from '@angular/core';
 import { Http, URLSearchParams } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/map';
 
 import { Article } from './article';
 import { environment } from '../environments/environment';
 
 @Injectable()
 export class ArticleService {
+  private _articles: BehaviorSubject<Article[]> =
+    new BehaviorSubject<Article[]>([]);
+
+  public articles: Observable<Article[]> = this._articles.asObservable();
 
   constructor(
     private http: Http
   ) { }
 
-  public getArticles(): Promise<Article[]> {
+  public getArticles(): void {
+    // make the http request -> Observable
+    // convert response into article class
+    // update our subject
+    this._makeHttpRequest('/v1/articles', 'reddit-r-all')
+      .map(json => json.articles)
+      .subscribe(articlesJSON => {
+        const articles = articlesJSON
+          .map(articlejson => Article.fromJSON(articlejson));
+        this._articles.next(articles);
+      });
+  }
+
+  private _makeHttpRequest(
+    path: string,
+    sourceKey: string
+  ): Observable<any> {
     const params = new URLSearchParams();
     params.set('apiKey', environment.newsApiKey);
-    params.set('source', 'reddit-r-all');
+    params.set('source', sourceKey);
 
     return this.http
-      .get(`${environment.baseUrl}/v1/articles`, {
+      .get(`${environment.baseUrl}${path}`, {
         search: params
-      })
-      .toPromise()
-      .then(resp => resp.json())
-      .then(json => json.articles)
-      .then(articles => {
-        console.log('json ->', articles);
-        return articles
-          .map(article =>
-          Article.fromJSON(article));
-      })
-      .catch(err => {
-        console.log('We got an error', err);
-      });
+      }).map(resp => resp.json());
   }
 }
