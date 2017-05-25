90f81538b855a5a69fefe81ab0be03e9b66c1955
Matt Wagner <matt@wags.us>
2017-05-25 10:15:45 -0400

    Add route to specific news source

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
+import { ArticleListComponent} from './article-list/article-list.component';
 
 const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: 'news/:sourcekey',
+    component: ArticleListComponent
   }
 ];
 
