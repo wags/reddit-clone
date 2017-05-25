7cd90ea51e2fd9ec76164e38e000985f6ab45b02
Matt Wagner <matt@wags.us>
2017-05-25 10:39:34 -0400

    Inject the article service in the sidebar component

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleService } from '../article.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  private sources: Observable<any>;
 
-  constructor() { }
+  constructor(
+    private articleService: ArticleService
+  ) {
+    this.sources = this.articleService.sources;
+  }
 
   ngOnInit() {
   }
